fc67624e403b9db51fb62ca58fadad3d8f2159ac
Mambulya <yashnova.anya@mail.ru>
2025-02-02 23:09:26 +0300

    tables are cropped correctly

--- DatabaseCourse2ORM/Form1.cs
+++ DatabaseCourse2ORM/Form1.cs
@@ -12,27 +12,54 @@ namespace DatabaseCourse2ORM
             dataGridViewORM.AutoGenerateColumns = true;
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
         private void btnPN_Click(object sender, EventArgs e)
         {
-            dataGridViewORM.DataSource = db.ProductionNormas.ToList();
+            dataGridViewORM.DataSource = db.ProductionNormas
+                .Select(p=> new { p.IdComponent, p.IdOperation, p.CodeWorkerProfession, 
+                    p.WorkerQuilification, p.TariffCodeWorker, p.CompleteTime, p.ItemTime})
+                .ToList();
         }
 
         private void btnWE_Click(object sender, EventArgs e)
         {
-            dataGridViewORM.DataSource = db.WorkerExperiences.ToList();
+            dataGridViewORM.DataSource = db.WorkerExperiences.
+                Select(we=> new { we.IdWorker, we.FinishedWorkDate, we.IdComponent,
+                we.IdOperation, we.NumGoodComponents, we.NumBadComponents, we.DefectProportion})
+                .ToList();
         }
 
         private void btnWI_Click(object sender, EventArgs e)
         {
-            dataGridViewORM.DataSource = db.WorkerInfos.ToList();
+            dataGridViewORM.DataSource = db.WorkerInfos.
+                Select(wi=> new {wi.IdWorker, wi.IdFactory, wi.IdDepartment,
+                wi.CodeWorkerProfession, wi.WorkerQuilification, wi.Married,
+                wi.Fio})
+                .ToList();
         }
 
         private void btnFill_Click(object sender, EventArgs e)
-        {
+        {   
+            // db.Database.ExecuteSql("sql query ...")
+
             try
             {
                 var quilification = int.Parse(textBoxQuilification.Text);
-                dataGridViewORM.DataSource = db.ProductionNormas.Where(q => q.WorkerQuilification > quilification).ToList();
+                dataGridViewORM.DataSource = db.ProductionNormas.Where(q => q.WorkerQuilification > quilification)
+                                    .Select(p => new
+                                    {
+                                        p.IdComponent,
+                                        p.IdOperation,
+                                        p.CodeWorkerProfession,
+                                        p.WorkerQuilification,
+                                        p.TariffCodeWorker,
+                                        p.CompleteTime,
+                                        p.ItemTime
+                                    }).ToList();
             } catch (Exception ex)
             {
                 MessageBox.Show("¬ведите корректный тип данных!");
