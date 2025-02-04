03d45e0906d97c6182e3e61c926e4680b5d43c94
Mambulya <yashnova.anya@mail.ru>
2025-02-04 23:00:20 +0300

    task 3 sql is done

--- DatabaseCourse2ORM/Exercise3SQL.cs
+++ DatabaseCourse2ORM/Exercise3SQL.cs
@@ -35,7 +35,9 @@ namespace DatabaseCourse2ORM
                              where p.ItemTime > 10
                              && p.CodeWorkerProfession == prof_parametr
                              && p.WorkerQuilification == q_parametr
-                             && !db.WorkerExperiences.Any(we => we.IdComponent == p.IdComponent && we.IdOperation == p.IdOperation && we.NumBadComponents > 0)
+                             && !db.WorkerExperiences.Any(we => we.IdComponent == p.IdComponent 
+                                                            && we.IdOperation == p.IdOperation 
+                                                            && we.NumBadComponents > 0)
                              select new
                              {
                                  p.IdComponent,
---
+++ DatabaseCourse2ORM/Models/Ex3SQL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseCourse2ORM.Models
+{
+    internal class Ex3SQL
+    {
+        public int IdComponent { get; set; }
+        public int IdOperation { get; set; }
+    }
+}
