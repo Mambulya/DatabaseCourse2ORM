9c20ed14dbbf8badd1d0286f531cd94d73d664f6
Mambulya <yashnova.anya@mail.ru>
2025-02-04 22:51:42 +0300

    linq-query

--- DatabaseCourse2ORM/Exercise3SQL.cs
+++ DatabaseCourse2ORM/Exercise3SQL.cs
@@ -26,7 +26,28 @@ namespace DatabaseCourse2ORM
 
         private void btnCalculate_Click_1(object sender, EventArgs e)
         {
-
+            try
+            {
+                int prof_parametr = int.Parse(txtProfession.Text);
+                int q_parametr = int.Parse(txtQ.Text);
+ 
+                var result = from p in db.ProductionNormas
+                             where p.ItemTime > 10
+                             && p.CodeWorkerProfession == prof_parametr
+                             && p.WorkerQuilification == q_parametr
+                             && !db.WorkerExperiences.Any(we => we.IdComponent == p.IdComponent && we.IdOperation == p.IdOperation && we.NumBadComponents > 0)
+                             select new
+                             {
+                                 p.IdComponent,
+                                 p.IdOperation
+                             };
+                var data = result.ToList();
+                dataGridViewEx3SQL.DataSource = data;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Введите корректные данные!");
+            }
         }
 
         private void dataGridViewEx3SQL_CellContentClick(object sender, EventArgs e)
